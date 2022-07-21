728ec188a615e359b649c2f9056154961d9b2111
2Yuni <andrei.plaschin@isa.utm.md>
2022-07-21 18:59:56 +0300

    test commit

---
+++ test/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
+        }
+    }
+}
